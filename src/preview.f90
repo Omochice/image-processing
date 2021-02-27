c98a0719160a948f4854e00b43ea6d4fe7e115b4
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-02-27 16:30:45 +0900

    update: add preview and run

--- src/preview.f90
+++ src/preview.f90
@@ -1,66 +1,36 @@
-program read_file
-  use filtering
+program preview
   use pnm_tools
-  implicit none    !未宣言変数の使用禁止
+  use filtering
+  use gradation_processing
+  use posterization
+  implicit none
   character(len=100) :: filename
-  character(len=2) :: pnm_type
-  integer :: img_width, img_height, img_depth
-  integer :: i = 0
-  integer :: iostatus = 1
-  integer :: w, h
-  integer, allocatable :: img_array(:, :)
-  integer, allocatable :: output_img(:, :)
-  integer, allocatable :: tmp(:, :)
+  integer, allocatable :: img(:, :, :)
+  integer :: maximum_value = 255
 
   call get_command_argument(1, filename)
-! <-- load pnm -->
-  open (10, file=filename, status="old", action="read", position="rewind")
-  read (10, *, iostat=iostatus) pnm_type
-  read (10, *, iostat=iostatus) img_width, img_height
-  read (10, *, iostat=iostatus) img_depth
-  print *, "img configs..."
-  print *, "format: ", pnm_type
-  print *, "w, h: ", img_width, img_height
 
-  allocate (img_array(img_width, img_height))
-  allocate (output_img(img_width, img_height))
-  allocate (tmp(img_width, img_height))
+  img = load_pnm(filename)
 
-  do i = 1, img_height
-    read (10, *, iostat=iostatus) img_array(:, i)
-    if (iostatus < 0) exit                         !最終行ならループを抜ける
-  end do
-  close (10)
-
-  call display_img(transpose(img_array), pnm_type, img_depth)
-
-!   print *, "noise rejection"
-!   call ITEN1(img_array, output_img, output_img, img_width, img_height, 3)
-!   call display_img(transpose(output_img), pnm_type, img_depth)
-
-  print *, "laplasian"
-! !   call lapf01(img_array, output_img, img_width, img_height, img_width, img_height)
-  call laplacian(img_array, output_img)
-  call display_img(transpose(output_img), pnm_type, img_depth)
-!
-  print *, "gaussian"
-  call gaussian(img_array, output_img, n_times=1)
-  call display_img(transpose(output_img), pnm_type, img_depth)
-!
-!   print *, "sobel"
-  print *, "canny edge detection"
-  call canny_edge_detection(img_array, output_img)
-  call display_img(transpose(output_img), pnm_type, img_depth)
-! !   call egsb2(output_img, workspace, img_array, img_width, img_height, 1)
-! !   call display_img(transpose(workspace), pnm_type, img_depth)
-!
-!
-  print *, "bilateral"
-  call bilateral(img_array, output_img, 0.3, 3)
-  call display_img(transpose(output_img), pnm_type, img_depth)
-
-!   print *, "edge"
-!   call egrb(img_array, output_img, img_width, img_height, 1)
-!   call display_img(transpose(output_img), pnm_type, img_depth)
-
-end program
+  call display_img(img, maximum_value)
+  ! filtering.f90
+  call display_img(laplacian(img, maximum_value), maximum_value)
+  call display_img(gaussian(img, maximum_value, 3), maximum_value)
+  call display_img(sobel(img, maximum_value, is_canny=.false.), maximum_value)
+  ! call display_img(canny_edge_detection(img, maximum_value), maximum_value)
+  call display_img(bilateral(img, 0.3, maximum_value, 3), maximum_value)
+  call display_img(emboss(img, maximum_value), maximum_value)
+  ! gradation_processing.f90
+  call display_img(linear_translation(img, maximum_value), maximum_value)
+  call display_img(brightness_translation(img, 45, maximum_value), maximum_value)
+  call display_img(contrast_translation(img, maximum_value, 0.3), maximum_value)
+  call display_img(gamma_correction(img, maximum_value, 3.0), maximum_value)
+  call display_img(histogram_equalization(img, maximum_value), maximum_value)
+  ! posterization.f08
+  call display_img(to_binary(img, 45), 1)
+  call display_img(otsu(img, maximum_value), 1)
+  call display_img(adaptive_threshold(img, maximum_value, 15), 1)
+  call display_img(quantize(img, 8, maximum_value), 8)
+  call display_img(dither(img, maximum_value), 1)
+  call display_img(error_diffusion(img, maximum_value), 1)
+end program preview
